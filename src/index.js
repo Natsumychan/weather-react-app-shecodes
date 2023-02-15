5b7863f9488e9659bc374ad25264b3c76defa132
Diana Castaño <dianaczapata@gmail.com>
2023-02-15 13:16:54 -0500

    add npm bootstrap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-
+import bootstrap from 'bootstrap' 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
