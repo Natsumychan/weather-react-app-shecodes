96e3ed63a92eb7ebf8331dd19a1aec250aeb9624
Diana Castaño <dianaczapata@gmail.com>
2023-02-15 12:51:50 -0500

    add title h1

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import './App.css';
 
 export default function App() {
   return (
-      <div className="App">
-        <h1>eather App</h1>
-        <footer>
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
         <p>
           This project was coded by Diana Castaño, and is an <a href="https://github.com/Natsumychan/weather-react-app-shecodes" target="_blank">open source</a>
         </p>
-        </footer>
-      </div>
-    );
-  }
+      </footer>
+    </div>
+  );
+}
 
 
 
