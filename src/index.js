b6a6641d4d9a08d9c38b05cd859ce26272b9d822
Diana Castaño <dianaczapata@gmail.com>
2023-02-26 12:51:11 -0500

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import bootstrap from 'bootstrap' 
+import "bootstrap/dist/css/bootstrap.css"; 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
