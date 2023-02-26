63ed65aa8f37f8ea26c69a32d436b64048157ea3
Diana Castaño <dianaczapata@gmail.com>
2023-02-26 17:44:58 -0500

    added timeline and weather static conditions

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./Weather.css"
 
-export default function weather (){
+export default function Weather (){
  return (
-  <div className="Weather">Hello from Weather</div>
+  <div className="weather">
+   <h1>New York</h1>
+   <ul>
+    <li>Wednesday 07:00</li>
+    <li>Mostly cloudy</li>
+   </ul>
+  </div>
  )
 }
