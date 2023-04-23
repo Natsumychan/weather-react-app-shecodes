8b68cd04f23cca9b9dd540e40be320037dbae1fd
Diana Castaño <dianaczapata@gmail.com>
2023-04-22 20:52:24 -0500

    package updated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import "bootstrap/dist/css/bootstrap.css"; 
-import './index.css';
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+// import Search from "./Search";
+import Weather from "./Weather"
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <Weather />
+  </StrictMode>
 );
