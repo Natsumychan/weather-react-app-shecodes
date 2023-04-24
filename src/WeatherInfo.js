bc3ef3dd01b7f9d9e4274e431b5efb63aeeb19ff
Diana Castaño <dianaczapata@gmail.com>
2023-04-23 22:13:07 -0500

    added WeatherTemperature component

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate  from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props){
  return <>
@@ -11,8 +12,7 @@ export default function WeatherInfo(props){
    <div className="row">
     <div className="col-6">
       <img src={props.info.icon} alt="clear"/>
-      <span className="temperature">{props.info.temperature}</span>
-      <span className="unit">°C</span>
+      <WeatherTemperature celsius={props.info.temperature}/>
     </div>
     <div className="col-6">
      <ul>
